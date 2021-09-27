bae6b3deae4fe16be99761287f42cbb7df02afd1
Konrad, Andres <andres.konrad@zuehlke.com>
2021-09-27 13:27:12 +0200

    Initial Commit

---
+++ src/test/java/ch/zuehlke/lambda/facilitator/FacilitatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zuehlke.lambda.facilitator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
