5ae137a713f3cfe0a520744d75ed916601453511
Konrad, Andres <andres.konrad@zuehlke.com>
2021-10-19 09:55:23 +0200

    Try to improve cors config

--- src/main/java/ch/zuehlke/lambda/facilitator/CorsConfig.java
+++ src/main/java/ch/zuehlke/lambda/facilitator/CorsConfig.java
@@ -8,9 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedMethods("POST", "PUT", "GET", "OPTIONS", "DELETE", "HEAD")
+                .allowedOrigins("*");
     }
 }
